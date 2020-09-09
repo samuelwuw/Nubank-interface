21c908e1f96ea00b313420e0a8d6e0e489aed2c4
Samuel Willian Alves Wu <samuelwuw@hotmail.com>
2020-09-09 16:00:08 -0300

    primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text:'Indicar amigos' },
+    { icon: 'phone-portrait-outline', text:'Recarga de celular' },
+    { icon: 'wallet-outline', text:'Depositar' },
+    { icon: 'options-outline', text:'Ajustar limite' },
+    { icon: 'help-circle-outline', text:'Me ajuda' },
+    { icon: 'barcode-outline', text:'Pagar' },
+    { icon: 'lock-open-outline', text:'Bloquear cartão' },
+    { icon: 'card-outline', text:'Cartão virtual' },
+  ];
+  public slidesOptions: any = { slidesPerView: 3, freeMode: true };
+  public itens: Array<any> = [
+    { icon: 'help-circle-outline', text: 'me ajuda'},
+    { icon: 'person-outline', text: 'perfil'},
+    { icon: 'cash-outline', text: 'Configurar conta'},
+    { icon: 'card-outline', text: 'Configurar cartão'},
+    { icon: 'phone-portrait-outline', text: 'Configurações de app'},
+  ];
 
   constructor() {}
 
